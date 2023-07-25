2b29f5e9592efec66afa08f82b9f0a1404c35621
luisescx <luis.esculapio@live.com>
2023-07-25 20:19:04 -0300

    chore: finish project

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
     return view('index', [
-      'tasks' => Task::latest()->get()
+      'tasks' => Task::latest()->paginate(10)
     ]);
 })->name('tasks.index');
 
@@ -61,6 +61,11 @@ Route::delete('/tasks/{task}', function (Task $task) {
 
   return redirect()->route('tasks.index')->with('success', 'Task deleted successfully');
 })->name('tasks.destroy');
+
+Route::put('tasks/{task}/toggle-complete', function(Task $task) {
+  $task->toggleComplete();
+  return redirect()->back()->with('success', 'Task updated successfully');
+})->name('tasks.toggle-complete');
 
 Route::fallback(function () {
   return "Nothing to show";
